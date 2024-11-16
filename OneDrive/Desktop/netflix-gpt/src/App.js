44e283e7c74fa268e5aa7f29d8e779e43789ef69
rajak <rajakoushik8008@gmail.com>
2024-11-16 11:54:28 +0530

    Initial commit

---
+++ OneDrive/Desktop/netflix-gpt/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App p-2 m-1 text-3xl font-bold bg-slate-400 text-gray-300">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
